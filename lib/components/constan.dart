b3a8a52480534b123df72c082166cdd277a120c8
gntrstwn <21sa1099@mhs.amikompurwokerto.ac.id>
2023-07-07 08:23:34 +0700

    full lagi

--- lib/components/constan.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-Color primaryButtonColor = Color.fromARGB(0, 255, 118, 34);
-Color whiteColor = Color.fromARGB(0, 255, 255, 255);
-Color textColor = Color.fromARGB(0, 100, 105, 130);
-Color textColorPrimary = Color.fromARGB(0, 50, 52, 62);
-
-// TextStyle orangeText = GoogleFonts.Sen(Colors: orangeText);
